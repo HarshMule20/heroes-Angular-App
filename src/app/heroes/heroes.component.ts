f21d3b8c7e7b1e38531b4253bce7373a1a1526e8
Harsh Mule <harsh.mule@extrapreneursindia.com>
2021-07-18 11:00:34 +0530

    feat: ngModel and ngFor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero = {
-    id : 111,
-    name: "Harsh Mule"
-  }
   constructor() { }
-  heroes = "this is heroes before"
-  heroes_array = ["this is first", "this is second", "this is third"]
+  heroes_dict = HEROES;
   ngOnInit() {
     this.test()
   }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,32 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {
+        id: 11,
+        name: "Ross Geller"
+    },
+    {
+        id: 12,
+        name: "Rachel Green"
+    },
+    {
+        id: 13,
+        name: "Joey Tribbiani"
+    },
+    {
+        id: 14,
+        name: "Chandler Bing"
+    },
+    {
+        id: 15,
+        name: "Monica Geller"
+    },
+    {
+        id: 16,
+        name: "Phoebe Buffay"
+    },
+    {
+        id: 17,
+        name: "Mike Hannigan"
+    },
+]
